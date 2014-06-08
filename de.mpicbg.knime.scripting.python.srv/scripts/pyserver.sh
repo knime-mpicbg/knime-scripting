543c05084cf820618dd6bf2590a2202bf5bc7b83
felix <nibelungen@gmx.ch>
2014-06-08 14:27:31 +0200

    Adapting the launch script to the new directory situation
    For now the script uses the default JRE of the operating system.

--- de.mpicbg.knime.scripting.python.srv/scripts/pyserver.sh
+++ de.mpicbg.knime.scripting.python.srv/scripts/pyserver.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-JAVA_HOME=/usr/java/jre1.6.0_23
-PYSERVER_HOME=/usr/local/bin/pyserver
+# Change directory to the server home (provided that this is executed from the scripts folder
+cd ..
+
+# Define a specific java RE here if necessairy here it' just the system default.
+JAVA=`which java`
 
 # This allows importing the ordereddict module for Python 2.6
+PYSERVER_HOME=`pwd`
 export PYTHONPATH=$PYSERVER_HOME:.
 
-CP=$PYSERVER_HOME/mpicbg-python-1.1.jar:\
-$PYSERVER_HOME/cajo-1.134.jar:\
-$PYSERVER_HOME/opencsv-2.1.jar
+# Defining the class path.
+CP=$PYSERVER_HOME/python-srv4knime.jar:\
+$PYSERVER_HOME/lib/cajo-1.134.jar:\
+$PYSERVER_HOME/lib/opencsv-2.1.jar
 
-$JAVA_HOME/bin/java -cp $CP de.mpicbg.sweng.pythonserver.PythonServer $*
+# Start the server. To stop it just kill the process.
+$JAVA -cp $CP de.mpicbg.knime.scripting.python.srv.PythonServer $*
