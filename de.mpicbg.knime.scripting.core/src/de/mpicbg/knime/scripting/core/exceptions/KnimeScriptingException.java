e54516ce68dd229dab3d8c9cc204f20d97dd71e6
Antje Janosch <niederle@mpi-cbg.de>
2014-07-30 15:34:43 +0200

    add KnimeScriptingException

---
+++ de.mpicbg.knime.scripting.core/src/de/mpicbg/knime/scripting/core/exceptions/KnimeScriptingException.java
@@ -0,0 +1,11 @@
+package de.mpicbg.knime.scripting.core.exceptions;
+
+public class KnimeScriptingException extends Exception {
+
+	/** a generated serial id. */
+	private static final long serialVersionUID = -872005723233752670L;
+
+	public KnimeScriptingException(final String message) {
+		super(message);
+	}
+}
