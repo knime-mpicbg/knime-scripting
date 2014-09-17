baf2de0eb4bcc44870ba3ef13b3ec836287aeb13
Antje Janosch <niederle@mpi-cbg.de>
2014-09-17 09:20:52 +0200

    helper methods to convert float array to double array and vice versa

--- de.mpicbg.knime.knutils/src/de/mpicbg/knime/knutils/Utils.java
+++ de.mpicbg.knime.knutils/src/de/mpicbg/knime/knutils/Utils.java
@@ -29,6 +29,44 @@ public class Utils {
         String os = System.getProperty("os.name");
         return os != null && os.equals("Mac OS X");
     }
+    
+    /**
+     * converts a float array into a double array
+     * @param input
+     * @return array with doubles
+     */
+    public static double[] convertFloatsToDoubles(float[] input)
+    {
+        if (input == null)
+        {
+            return null;
+        }
+        double[] output = new double[input.length];
+        for (int i = 0; i < input.length; i++)
+        {
+            output[i] = input[i];
+        }
+        return output;
+    }
+    
+    /**
+     * converts a double array into a float array; USE WITH CAUTION as precision might be lost! 
+     * @param input
+     * @return array with doubles
+     */
+    public static float[] convertDoublesToFloats(double[] input)
+    {
+        if (input == null)
+        {
+            return null;
+        }
+        float[] output = new float[input.length];
+        for (int i = 0; i < input.length; i++)
+        {
+            output[i] = (float) input[i];
+        }
+        return output;
+    }
 
     /**
      * retrieves the KNIME DataType corresponding to a java class
