512fefa01a2402df60cab41fa4b1363b4f4d64f4
Antje Janosch <niederle@mpi-cbg.de>
2015-08-24 10:36:08 +0200

    move openIn: work in progress

--- de.mpicbg.knime.scripting.r/src/de/mpicbg/knime/scripting/r/RSnippetNodeModel.java
+++ de.mpicbg.knime.scripting.r/src/de/mpicbg/knime/scripting/r/RSnippetNodeModel.java
@@ -1,9 +1,11 @@
 package de.mpicbg.knime.scripting.r;
 
+import de.mpicbg.knime.knutils.Utils;
 import de.mpicbg.knime.scripting.core.AbstractTableScriptingNodeModel;
 import de.mpicbg.knime.scripting.core.exceptions.KnimeScriptingException;
 import de.mpicbg.knime.scripting.r.prefs.RPreferenceInitializer;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataType;
 import org.knime.core.node.BufferedDataTable;
@@ -18,6 +20,12 @@ import org.rosuda.REngine.REXPString;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -131,15 +139,79 @@ public class RSnippetNodeModel extends AbstractTableScriptingNodeModel {
 	@Override
 	protected void openIn(BufferedDataTable[] inData, ExecutionContext exec)
 			throws KnimeScriptingException {
-		throw new KnimeScriptingException("not yet implemented");
-	}
+		//throw new KnimeScriptingException("not yet implemented");
+		
+        try {
 
-/*
-	@Override
-	protected BufferedDataTable[] executeImpl(BufferedDataTable[] inData,
-			ExecutionContext exec) throws KnimeScriptingException {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
+            logger.info("Creating R-connection");
+            RConnection connection = RUtils.createConnection();
+
+            // 1) convert exampleSet ihnto data-frame and put into the r-workspace
+            logger.info("Pushing inputs to R...");
+
+            Map<String, Object> pushTable = RUtils.pushToR(inData, connection, exec);
+
+            // save the work-space to a temporary file and open R
+
+            String allParams = pushTable.keySet().toString().replace("[", "").replace("]", "").replace(" ", "");
+
+            connection.voidEval("tmpwfile = tempfile('openinrnode', fileext='.RData');");
+            connection.voidEval("save(" + allParams + ", file=tmpwfile); ");
+
+
+            // 2) transfer the file to the local computer if necessary
+            logger.info("Transferring workspace-file to localhost ...");
+
+            File workspaceFile = null;
+            if (RUtils.getHost().equals("localhost")) {
+                workspaceFile = new File(connection.eval("tmpwfile").asString());
+            } else {
+                // create another local file  and transfer the workspace file from the rserver
+                workspaceFile = File.createTempFile("rplugin", ".RData");
+                workspaceFile.deleteOnExit();
+
+                REXP xp = connection.parseAndEval("r=readBin(tmpwfile,'raw',file.info(tmpwfile)$size); unlink(tmpwfile); r");
+                FileOutputStream oo = new FileOutputStream(workspaceFile);
+                oo.write(xp.asBytes());
+                oo.close();
+            }
+
+            connection.voidEval("rm(list = ls(all = TRUE));");
+            connection.close();
+
+            logger.info("Spawning R-instance ...");
+            openWSFileInR(workspaceFile, prepareScript());
+
+
+            setWarningMessage("To push the node's input to R again, you need to reset and re-execute it.");
+
+        } catch (Throwable e) {
+            throw new RuntimeException("spawning of R-process failed", e);
+        }
+	}
+	
+    public static void openWSFileInR(File workspaceFile, String script) throws IOException {
+        IPreferenceStore prefStore = R4KnimeBundleActivator.getDefault().getPreferenceStore();
+        String rExecutable = prefStore.getString(RPreferenceInitializer.LOCAL_R_PATH);
+
+        // 3) spawn a new R process
+        if (Utils.isWindowsPlatform()) {
+            Runtime.getRuntime().exec(rExecutable + " " + workspaceFile.getAbsolutePath());
+        } else if (Utils.isMacOSPlatform()) {
+
+            Runtime.getRuntime().exec("open -n -a " + rExecutable + " " + workspaceFile.getAbsolutePath());
+
+        } else { // linux and the rest of the world
+            Runtime.getRuntime().exec(rExecutable + " " + workspaceFile.getAbsolutePath());
+        }
+
+        // copy the script in the clipboard
+        if (!script.isEmpty()) {
+            StringSelection data = new StringSelection(script);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(data, data);
+        }
+    }
+
 }
 
