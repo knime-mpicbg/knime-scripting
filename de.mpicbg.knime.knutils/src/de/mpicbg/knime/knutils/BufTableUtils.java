c61f1164aa9ae9f3cfff09e4e8a9caddbbf62083
Antje Janosch <niederle@mpi-cbg.de>
2015-11-24 16:11:36 +0100

    knutils: add method to create new KNIME table with given columns + types

--- de.mpicbg.knime.knutils/src/de/mpicbg/knime/knutils/BufTableUtils.java
+++ de.mpicbg.knime.knutils/src/de/mpicbg/knime/knutils/BufTableUtils.java
@@ -1,13 +1,24 @@
 package de.mpicbg.knime.knutils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataTableSpecCreator;
+import org.knime.core.data.DataType;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -64,5 +75,26 @@ public class BufTableUtils {
     public static void updateProgress(ExecutionContext exec, long done, long all) throws CanceledExecutionException {
         exec.checkCanceled();
         exec.setProgress((double) done / (double) all);
+    }
+    
+    /**
+     * creates a data table spec based on a hash map with column names and types
+     * @param columns
+     * @return new data table spec
+     */
+    public static DataTableSpec createNewDataTableSpec(LinkedHashMap<String, DataType> columns) {
+    	DataColumnSpecCreator colSpecCreator = null;
+    	DataColumnSpec[] cSpecList = new DataColumnSpec[columns.size()];
+    	int i = 0;
+    	for(String columnName : columns.keySet()) {
+    		colSpecCreator = new DataColumnSpecCreator(columnName, columns.get(columnName));
+    		cSpecList[i] = colSpecCreator.createSpec();
+    		i++;
+    	}
+    	
+    	DataTableSpecCreator specCreator = new DataTableSpecCreator();
+    	specCreator.addColumns(cSpecList);
+    	
+    	return specCreator.createSpec();
     }
 }
