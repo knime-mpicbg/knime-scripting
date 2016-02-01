745174713589b8bb3d1dd2e914c2049c1571b3d3
Antje Janosch <niederle@mpi-cbg.de>
2016-02-01 13:35:16 +0100

    retrieve back old ConvertToGeneric node (backward compatibility)

---
+++ de.mpicbg.knime.scripting.r/src/de/mpicbg/knime/scripting/r/generic/ConvertToGenericR.java
@@ -0,0 +1,76 @@
+package de.mpicbg.knime.scripting.r.generic;
+
+import de.mpicbg.knime.knutils.AbstractNodeModel;
+import de.mpicbg.knime.scripting.r.RSnippetNodeModel;
+import de.mpicbg.knime.scripting.r.RUtils;
+
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.rosuda.REngine.Rserve.RConnection;
+
+import java.io.File;
+
+import static de.mpicbg.knime.scripting.r.RSnippetNodeModel.R_INVAR_BASE_NAME;
+import static de.mpicbg.knime.scripting.r.RSnippetNodeModel.R_OUTVAR_BASE_NAME;
+
+
+/**
+ * A generic R node which transforms generic R objects and not just BufferedDataTables (aka. data frames)
+ *
+ * @author Holger Brandl
+ */
+public class ConvertToGenericR extends AbstractNodeModel {
+
+    private File rWorkspaceFile;
+
+
+    public ConvertToGenericR() {
+        // the input port is optional just to allow generative R nodes
+        super(createPorts(1, BufferedDataTable.TYPE, BufferedDataTable.class), createPorts(1, RPortObject.TYPE, RPortObject.class));
+    }
+
+
+    @Override
+    protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
+
+        RConnection connection = RUtils.createConnection();
+
+        try {
+        // 1) onvert the data and push them to R
+        RUtils.pushToR(inObjects, connection, exec);
+
+        connection.voidEval(R_OUTVAR_BASE_NAME + " <- " + R_INVAR_BASE_NAME);
+
+
+        // 2) write a local workspace file which contains the input table of the node
+        if (rWorkspaceFile == null) {
+            rWorkspaceFile = File.createTempFile("genericR", "R");  //Note: this r is just a filename suffix
+        }
+
+        RUtils.saveToLocalFile(rWorkspaceFile, connection, RUtils.getHost(), RSnippetNodeModel.R_OUTVAR_BASE_NAME);
+        } catch(Exception e) {
+        	connection.close();
+        	throw e;
+        }
+
+        connection.close();
+
+        return new PortObject[]{new RPortObject(rWorkspaceFile)};
+    }
+
+
+    @Override
+    // note: This is not the usual configure but a more generic one with PortObjectSpec instead of DataTableSpec
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        return new PortObjectSpec[]{RPortObjectSpec.INSTANCE};
+    }
+
+
+    @Override
+    protected BufferedDataTable[] execute(BufferedDataTable[] inData, ExecutionContext exec) throws Exception {
+        throw new RuntimeException("fake implementation: This method should be never called");
+    }
+}
