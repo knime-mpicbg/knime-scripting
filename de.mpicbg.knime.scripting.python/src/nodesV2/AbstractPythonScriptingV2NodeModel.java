91d7164f437c088d4ec82bf4dfe28aa1cf2ff222
Antje Janosch <niederle@mpi-cbg.de>
2019-03-13 11:27:10 +0100

    Python Scripting: start to write new version of python nodes

---
+++ de.mpicbg.knime.scripting.python/src/nodesV2/AbstractPythonScriptingV2NodeModel.java
@@ -0,0 +1,232 @@
+package nodesV2;
+
+import de.mpicbg.knime.knutils.Utils;
+import de.mpicbg.knime.scripting.core.AbstractScriptingNodeModel;
+import de.mpicbg.knime.scripting.core.ScriptingModelConfig;
+import de.mpicbg.knime.scripting.core.exceptions.KnimeScriptingException;
+import de.mpicbg.knime.scripting.core.rgg.TemplateUtils;
+import de.mpicbg.knime.scripting.python.PythonColumnSupport;
+import de.mpicbg.knime.scripting.python.PythonScriptingBundleActivator;
+import de.mpicbg.knime.scripting.python.prefs.PythonPreferenceInitializer;
+import de.mpicbg.knime.scripting.python.srv.LocalPythonClient;
+import de.mpicbg.knime.scripting.python.srv.Python;
+import de.mpicbg.knime.scripting.python.srv.PythonTempFile;
+import de.mpicbg.knime.scripting.r.RColumnSupport;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortType;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.io.*;
+
+
+public abstract class AbstractPythonScriptingV2NodeModel extends AbstractScriptingNodeModel {
+	
+	public static final String R_INVAR_BASE_NAME = "kIn";
+	public static final String R_OUTVAR_BASE_NAME = "pyOut";
+	
+	public static final String CFG_SCRIPT_DFT = "pyOut = kIn";
+	
+    // Temp files for reading/writing the table and the script
+    protected PythonTempFile kInFile;
+    protected PythonTempFile pyOutFile;
+    protected PythonTempFile scriptFile;
+
+    protected Python python;
+
+    protected IPreferenceStore preferences = PythonScriptingBundleActivator.getDefault().getPreferenceStore();
+
+    /**
+	 * @param inPorts
+	 * @param outPorts
+	 * @param rColumnSupport
+	 */
+	public AbstractPythonScriptingV2NodeModel(PortType[] inPorts, PortType[] outPorts, PythonColumnSupport rColumnSupport) {
+		super(inPorts, outPorts, rColumnSupport);
+	}
+
+	/**
+	 * 
+	 * @param inPorts
+	 * @param outPorts
+	 */
+	public AbstractPythonScriptingV2NodeModel(PortType[] inPorts, PortType[] outPorts) {
+		super(inPorts, outPorts, new PythonColumnSupport());
+	}
+
+	/**
+	 * constructor with node configuration object
+	 * @param nodeModelConfig
+	 */
+	public AbstractPythonScriptingV2NodeModel(ScriptingModelConfig nodeModelConfig) {
+		super(nodeModelConfig);
+	}
+	
+	/**
+	 * main method to push available input to R
+	 * NOTE: method does not close the connection in case of exceptions
+	 * 
+	 * @param inData
+	 * @param exec
+	 * @throws KnimeScriptingException
+	 * @throws CanceledExecutionException
+	 */
+	protected void pushInputToPython(PortObject[] inData, ExecutionContext exec) 
+			throws KnimeScriptingException, CanceledExecutionException {
+		
+		ScriptingModelConfig cfg = getNodeCfg();
+	}
+
+	/** ============================================================================================= */
+
+    protected void prepareScript(Writer writer, boolean useScript) throws IOException {
+        // CSV read/write functions
+        //InputStream utilsStream = PythonScripts.class.getResourceAsStream("PythonCSVUtils.py");
+    	InputStream utilsStream = getClass().getClassLoader().getResourceAsStream("/de/mpicbg/knime/scripting/python/scripts/PythonCSVUtils.py");
+
+
+        String readCSVCmd = kInFile != null ? "kIn, columnTypes = read_csv(r\"" + kInFile.getServerPath() + "\", True)" : "";
+        String writeCSVCmd = pyOutFile != null ? "write_csv(r\"" + pyOutFile.getServerPath() + "\", pyOut, True)" : "";
+
+        // Write the script file
+        writer.write(TemplateUtils.convertStreamToString(utilsStream));
+        writer.write("\n" + readCSVCmd + "\n");
+
+        // Insert the user-defined script here
+        if(useScript)
+        	writer.write("\n" + super.prepareScript() + "\n");
+
+        writer.write("\n" + writeCSVCmd + "\n");
+
+        return;
+    }
+
+    @Override
+    public String prepareScript() throws RuntimeException {
+        try {
+            Writer writer = new BufferedWriter(new FileWriter(scriptFile.getClientFile()));
+            try {
+                prepareScript(writer, true);
+            } finally {
+                writer.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        scriptFile.upload();
+        return scriptFile.getServerPath();
+    }
+
+    /**
+     * Create necessary temp files
+     */
+    protected void createTempFiles() throws RuntimeException {
+        try {
+            // Delete the previous set if they're still around
+            deleteTempFiles();
+
+            // Create a new set
+            kInFile = new PythonTempFile(python, "knime2python", ".csv");
+            pyOutFile = new PythonTempFile(python, "python2knime", ".csv");
+            scriptFile = new PythonTempFile(python, "analyze", ".py");
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Delete all temp files if they exist and the node is so configured
+     */
+    protected void deleteTempFiles() {
+        if (kInFile != null) kInFile.delete();
+        if (pyOutFile != null) pyOutFile.delete();
+        if (scriptFile != null) scriptFile.delete();
+    }
+
+    /**
+     * opens python externally and loads KNIME input data, script is put into clipboard
+     * @param inData
+     * @param exec
+     * @param logger
+     * @throws KnimeScriptingException
+     */
+    protected void openInPython(PortObject[] inData, ExecutionContext exec, NodeLogger logger) throws KnimeScriptingException {
+    	IPreferenceStore preferences = PythonScriptingBundleActivator.getDefault().getPreferenceStore();
+
+    	//      boolean local = preferences.getBoolean(PythonPreferenceInitializer.PYTHON_LOCAL);
+    	//      if (!local) throw new RuntimeException("This node can only be used with a local python executable");
+
+    	python = new LocalPythonClient();
+
+    	createTempFiles();
+    	pyOutFile = null;
+
+    	// Write data into csv
+    	BufferedDataTable[] inTables = castToBDT(inData);
+    	logger.info("Writing table to CSV file");
+    	PythonTableConverter.convertTableToCSV(exec, inTables[0], kInFile.getClientFile(), logger);
+
+    	// Create and execute script
+    	String pythonExecPath = preferences.getString(PythonPreferenceInitializer.PYTHON_EXECUTABLE);
+
+    	// get the full path of the python executable for MacOS
+    	String pythonExecPathFull = pythonExecPath;
+    	try {
+    		if (Utils.isMacOSPlatform()) {
+    			Runtime r = Runtime.getRuntime();
+    			Process p = r.exec("which " + pythonExecPath);
+    			p.waitFor();
+    			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+    			pythonExecPathFull = reader.readLine();
+    		}
+    	} catch (Exception e) {
+    		logger.error(e);
+    	}
+
+    	try {
+    		Writer writer = new BufferedWriter(new FileWriter(scriptFile.getClientFile()));
+    		try {
+    			// Write a shebang to invoke the python interpreter 
+    			writer.write("#! " + pythonExecPathFull + " -i\n");
+    			prepareScript(writer, false);
+    		} finally {
+    			writer.close();
+    		}
+
+    		scriptFile.getClientFile().setExecutable(true);
+
+    		// Run the script
+    		if (Utils.isMacOSPlatform()) {
+    			Runtime.getRuntime().exec("open -a Terminal " + " " + scriptFile.getClientPath());
+    		} else if (Utils.isWindowsPlatform()) {
+    			Runtime.getRuntime().exec(new String[] {
+    					"cmd",
+    					"/k",
+    					"start",
+    					pythonExecPath,
+    					"-i",
+    					"\"" + scriptFile.getClientPath() + "\""
+    			});
+    		} else logger.error("Unsupported platform");
+    		
+    		// copy the script in the clipboard
+    		String actualScript = super.prepareScript();
+            if (!actualScript.isEmpty()) {
+                StringSelection data = new StringSelection(actualScript);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(data, data);
+            }
+    		
+    	} catch (Exception e) {
+    		throw new KnimeScriptingException("Failed to open in Python\n" + e);
+    	}
+    }
+}
